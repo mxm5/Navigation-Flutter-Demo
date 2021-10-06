c89391f08307414e1462fc9fccb03d0ee66c216f
vanil adra <vaniladra@gmail.com>
2021-10-06 17:31:38 +0330

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'screen1.dart';
+import 'screen0.dart';
+import 'screen2.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,7 +9,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Screen1(),
+//      home: Screen0(),
+// cant use initialRoute cuz it takes str name
+//    and home attr conflicts with the
+//    route definition ,
+    initialRoute: '/',
+      // not a widget
+      routes: {
+        '/': (context) => Screen0(), //so this shit keys VALUE is some
+        //// widget builder context
+        '/screen1': (context) => Screen1(),
+        '/screen2': (context) => Screen2(),
+        //// a widget builder is a TYPE that is a
+        //// ...Function TYPE  that ...
+        //// receives a ( ...context ) and
+        //// returns => a ...Widget TYPE
+        ////...............................////
+      },
     );
   }
 }
